537633e19874cf88ec99b7b55a0fbdf084bdce58
JBaronOsorio <jjbarono@eafit.edu.co>
2026-03-23 15:01:22 -0500

    Add about and contact pages with templates, views and routes

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import HttpResponse
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name= 'pages/home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page ...",
+            "author": "Developed by: Juan José Baron Osorio",
+        })
+        return context
+    
+class ContactPageView(TemplateView):
+    template_name = 'pages/contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "Contact - Online Store",
+            "subtitle": "Contact us",
+            "email": "contact@onlinestore.com",
+            "address": "123 Main Street, New York, USA",
+            "phone": "+1 234 567 890"
+        })
+        return context
